8bc16b14bd827d833ff396b7d41ccb79335b33e2
minhtien2152 <18521481@gm.uit.edu.vn>
2021-06-14 11:23:37 +0700

    add buttons

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-
+import React, { useRef, useState } from "react";
+import "@fortawesome/fontawesome-free/css/all.css";
 import Editor from "react-simple-code-editor";
 import dedent from "dedent";
 import { highlight, languages } from "prismjs/components/prism-core";
@@ -7,10 +7,12 @@ import "prismjs/components/prism-clike";
 import "prismjs/components/prism-javascript";
 import "prismjs/components/prism-markup";
 //import "./styles.css";
+import fs from "fs";
 import "./codeEditor.css";
 import { generator } from "./separator";
 import CodeEditor from "./CodeEditor";
 const App = () => {
+  const ref = useRef();
   const [code, setCode] = useState(dedent`
   import React from "react";
   import ReactDOM from "react-dom";
@@ -21,12 +23,53 @@ const App = () => {
   }
   ReactDOM.render(<App />, document.getElementById("root"));
   `);
+
   const [result, setResult] = useState("");
+
+  const fileChangeHandler = (e) => {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      setCode(e.target.result);
+    };
+    console.log(e.target.files);
+    reader.readAsText(e.target.files[0]);
+  };
+
+  const handleOpenFile = () => {
+    ref.current.click();
+  };
+  const handleClose = () => {
+    window.close();
+  };
+  const handleNew = () => {
+    setCode("");
+  };
+
+  const renderHeadBar = () => {
+    return (
+      <>
+        <span class="control" onClick={handleNew}>
+          <i class="fas fa-file"></i>
+        </span>
+        <span class="control" onClick={handleOpenFile}>
+          <i class="fas fa-folder-open"></i>
+        </span>
+        <span class="control" onClick={handleClose}>
+          <i class="fas fa-times"></i>
+        </span>
+      </>
+    );
+  };
 
   return (
     <main className="container">
-      <button onClick={() => console.log(code)}>a</button>
-      <CodeEditor>
+      <input
+        ref={ref}
+        type="file"
+        style={{ visibility: "hidden" }}
+        onChange={fileChangeHandler}
+      />
+      <CodeEditor headbar={renderHeadBar}>
         <Editor
           placeholder="Type some code…"
           value={code}
--- src/CodeEditor.js
+++ src/CodeEditor.js
@@ -1,13 +1,9 @@
 import React from "react";
 import "./codeEditor.css";
-const CodeEditor = ({ children }) => {
+const CodeEditor = ({ children, headbar }) => {
   return (
     <div class="code-editor">
-      <div className="head-bar">
-        <span class="control"></span>
-        <span class="control"></span>
-        <span class="control"></span>
-      </div>
+      <div className="head-bar">{headbar && headbar()}</div>
       <div className="content">
         <pre className="line-numbers">{children}</pre>
       </div>
